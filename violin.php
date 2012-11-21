dc1460e53663878af85f6a47f000f36ac4a58a80
kobitoino <kobito0826@gmail.com>
2012-11-22 02:56:19 +0900

    violin.php追加

---
+++ violin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="chrome=1,IE=9" />
+<meta http-equiv="X-UA-Compatible" content="IE=8" />
+<title>Haus-de-musik（ハウスデムジカ）</title>
+<meta content="バイオリン,ピアノ,教室,横浜,追浜" name="keywords" />
+<meta content="" name="description" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<link rel="icon" href="image/base/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="image/base/favicon.ico" type="image/x-icon" />
+<link type="text/css" rel="stylesheet" href="css/base.css" media="all" charset="utf-8" />
+<link type="text/css" rel="stylesheet" href="css/index.css" media="all" charset="utf-8" />
+<link type="text/css" rel="stylesheet" href="css/violin.css" media="all" charset="utf-8" />
+<script type='text/javascript' src='js/jquery-1.8.2.min.js'></script>
+<script type='text/javascript' src='js/hdm.js'></script>
+<script type='text/javascript' src='js/hdm/index.js'></script>
+</head>
+<body class="index">
+	<div class="wrapper">
+    <div class="contents">
+        <? require_once 'partials/header.php'; ?>
+      <div class="key-visual">      
+      </div><!-- /key-visual -->
+        <? require_once 'partials/footer.php'; ?> 
+    </div><!-- /contents -->
+	</div><!-- /wrapper -->
+</body>
+</html>
