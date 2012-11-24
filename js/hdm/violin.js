14deb043e9e7e7bd0ed04cc3bc77e58a619df0dc
ishikawa-h <ishikawa@toshin-k.com>
2012-11-25 00:20:28 +0900

    violin.php js実装

---
+++ js/hdm/violin.js
@@ -0,0 +1,43 @@
+(function () {
+  if (typeof window.hdm === 'undefined') {
+    window.hdm = {};
+  }
+
+  if (typeof window.hdm.violin === 'undefined') {
+    window.hdm.violin = {};
+  }
+
+  var v = window.hdm.violin;
+
+  v.toggle = function (tag) {
+    if(tag == '1') {
+      $('.box001').show();
+      $('.box002').hide();
+      $('.box003').hide();
+      $('.selected').removeClass('selected');
+      $('.navi00' + tag).addClass('selected');
+    } else if(tag == '2') {
+      $('.box002').show();
+      $('.box001').hide();
+      $('.box003').hide();
+      $('.selected').removeClass('selected');
+      $('.navi00' + tag).addClass('selected');
+    } else if(tag == '3') {
+      $('.box003').show();
+      $('.box001').hide();
+      $('.box002').hide();
+      $('.selected').removeClass('selected');
+      $('.navi00' + tag).addClass('selected');
+    }
+
+
+  };
+}());
+
+/*
+$(function () {
+  $('p').click(function () {
+    $(this).next('span').slideToggle();
+  });
+});
+*/
