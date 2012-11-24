7a5d01527c225d053e738fc806d6aa104734cbc3
ishikawa-h <ishikawa@toshin-k.com>
2012-11-24 19:03:25 +0900

    FB導入

--- index.php
+++ index.php
@@ -52,6 +52,7 @@
       <div class="info">
         <div class="fb">
           <span>更新情報 from facebook</span>
+          <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FHausDeMusik&amp;width=330&amp;height=245&amp;colorscheme=light&amp;show_faces=false&amp;border_color&amp;stream=true&amp;header=true&amp;appId=337089699705595" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:330px; height:245px;" allowTransparency="true"></iframe>
         </div><!-- /fb -->
         <div class="request">
           <a href=""><p></p></a>
