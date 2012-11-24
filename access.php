55e9f1c0374404d2ef14c223175605513040b54c
ishikawa-h <ishikawa@toshin-k.com>
2012-11-24 18:29:54 +0900

    アクセスページ追加

---
+++ access.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="chrome=1,IE=9" />
+<meta http-equiv="X-UA-Compatible" content="IE=8" />
+<title>Haus-de-musik（ハウスデムジカ）| アクセス</title>
+<meta content="バイオリン,ピアノ,教室,横浜,追浜" name="keywords" />
+<meta content="" name="description" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<link rel="icon" href="image/base/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="image/base/favicon.ico" type="image/x-icon" />
+<link type="text/css" rel="stylesheet" href="css/base.css" media="all" charset="utf-8" />
+<link type="text/css" rel="stylesheet" href="css/access.css" media="all" charset="utf-8" />
+<script type='text/javascript' src='js/jquery-1.8.2.min.js'></script>
+<script type='text/javascript' src='js/hdm.js'></script>
+<script type='text/javascript' src='js/hdm/index.js'></script>
+</head>
+<body class="access">
+	<div class="wrapper">
+    <div class="contents">
+        <? require_once 'partials/header.php'; ?>
+      <div class="title">
+        <div>
+          <img src="images/base/violin.png" width="38" height="35" alt="バイオリン" />
+          <img src="images/base/piano.png" width="36" height="47" alt="ピアノ" />
+          <span>アクセス</span>
+        </div>          
+      </div><!-- /title -->
+      <div class="map">
+        <p>
+          <span>Haus-de-musik Aoyama</span>
+          <ul>
+            <li><span>所在地：</span>神奈川県横須賀市湘南鷹取3丁目</li>
+            <li><span>最寄り駅：</span>京浜急行「追浜駅」</li>
+            <li class="indent">追浜駅より「湘南鷹取循環」または「鷹取一丁目」バスに乗車。 「鷹取中学校前」下車徒歩５分。</li>
+            <li class="indent">内川橋ー安浦利用の場合は「追浜一丁目」のバス停下車徒歩７分。</li>
+            <li class="indent">※お車の方は駐車場スペースあり。</li>
+          </ul>
+        </p>
+        <iframe width="950" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.jp/maps?f=q&amp;source=s_q&amp;hl=ja&amp;geocode=&amp;q=%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C%E6%A8%AA%E9%A0%88%E8%B3%80%E5%B8%82%E6%B9%98%E5%8D%97%E9%B7%B9%E5%8F%963%E4%B8%81%E7%9B%AE&amp;aq=&amp;sll=34.728949,138.455511&amp;sspn=44.022063,56.601563&amp;brcurrent=3,0x60184131beff37d3:0x72c7da9579ccbb47,0&amp;ie=UTF8&amp;hq=&amp;hnear=%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C%E6%A8%AA%E9%A0%88%E8%B3%80%E5%B8%82%E6%B9%98%E5%8D%97%E9%B7%B9%E5%8F%96%EF%BC%93%E4%B8%81%E7%9B%AE&amp;ll=35.305459,139.617237&amp;spn=0.002705,0.003455&amp;t=m&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>      
+      </div><!-- /map -->
+        <? require_once 'partials/footer.php'; ?> 
+    </div><!-- /contents -->
+	</div><!-- /wrapper -->
+</body>
+</html>
