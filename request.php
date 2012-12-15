b13b9d6319edd866bf22c6e741be6919eeaa44d7
kobitoino <kobito0826@gmail.com>
2012-12-15 11:10:55 +0900

    演奏依頼追加

---
+++ request.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="chrome=1,IE=9" />
+<meta http-equiv="X-UA-Compatible" content="IE=8" />
+<title>Haus-de-musik（ハウスデムジカ）</title>
+<meta content="バイオリン,ピアノ,教室,横浜,追浜" name="keywords" />
+<meta content="" name="description" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<link rel="icon" href="image/base/favicon.ico" type="image/x-icon" />
+<link rel="shortcut icon" href="image/base/favicon.ico" type="image/x-icon" />
+<link type="text/css" rel="stylesheet" href="css/base.css" media="all" charset="utf-8" />
+<link type="text/css" rel="stylesheet" href="css/request.css" media="all" charset="utf-8" />
+<link href="css/lightbox.css" rel="stylesheet" />
+<script type='text/javascript' src='js/jquery-1.8.2.min.js'></script>
+<script type='text/javascript' src='js/hdm.js'></script>
+<script type='text/javascript' src='js/hdm/violin.js'></script>
+<script src="js/lightbox/jquery-1.7.2.min.js"></script>
+<script src="js/lightbox/lightbox.js"></script>
+</head>
+<body class="index">
+<div class="wrapper">
+  <div class="contents">
+    <? require_once 'partials/header.php'; ?>
+      <div class="inner">
+      <div class="title">
+        <div>
+          <img src="images/base/violin.png" width="38" height="35" alt="バイオリンアイコン" /><span>演奏依頼</span>
+        </div>          
+      </div><!-- /title -->
+      <div class="big_box">
+        <div class="box_left">
+          <div class="menu_left">
+            <ul>
+              <li class="navi001 selected" onclick="window.hdm.violin.toggle(1)"><img src="images/base/treble-clef.png" width="10" height="20" alt="音符" /><span>HOME</span></li>
+              <li class="navi002" onclick="window.hdm.violin.toggle(2)"><img src="images/base/treble-clef.png" width="10" height="20" alt="音符" /><span>演奏履歴</span></li>
+              <li class="navi003" onclick="window.hdm.violin.toggle(3)"><img src="images/base/treble-clef.png" width="10" height="20" alt="音符" /><span>出張コンサート歴</span></li>
+              <li class="navi005" onclick="window.hdm.violin.toggle(5)"><img src="images/base/treble-clef.png" width="10" height="20" alt="音符" /><span>演奏依頼</span></li>
+            </ul>
+          </div><!-- /menu_left -->
+        </div><!-- /box_left -->
+        <div class="box_main">
+          <div class="box001">
+          <div class="box_right">
+            <p class="obi"><b>プロの演奏をお探しの方</b></p>
+          </div><!-- /box_right -->
+          <div class="cb"></div>
+            <div class="request">
+                	
+                	<div class="quartet"><img src="images/violin/Ｄjyanru01.jpg" />
+                    </div>
+
+              <p class="request_sentence">
+              <b>プロの演奏家をお探しの方はHaus-de-musik Aoyamaへ！</b><br /><br />
+プロの演奏家の出張演奏いたしています。<br />
+                桐朋、芸大卒などの音楽仲間と伺えたら・・・と思っています。<br />
+                演奏に対しては厳しく鍛えられ、きちんとした気持ちで臨むことを学んできた仲間ばかりですので、安心してお任せください。<br />
+                <br />
+                新曲も含め、あらゆるジャンル(クラシック、ポピュラー、日本の曲、アニメソング、童謡、ジャズ、タンゴなど)の曲に、楽譜を入手後、数日で本番に臨めます。<br />
+                <br />
+                演奏時間、構成などはご希望に合わせますのでご相談ください。<br />
+                大切な場の雰囲気にふさわしい演奏となるよう、様々なレパートリーの中から選曲いたします。リクエストがある場合には、どうぞお気軽にお問い合わせください。<br />
+                <br />
+                各種パーティー、宴会、レセプションに多数の出演実績があり、様々な著名アーティストのレコーディング、コンサートの<br />
+バックミュージシャンとしての参加経験多数。<br />
+                <br />
+                スタジオやコンサートホール、レコーディング、ライブハウス、レストラン、大使館、迎賓館、公園、ドームなど出演多数。</p>
+</div><!-- /sentence01 -->
+            </div><!-- /box001 -->
+            
+            <div class="box002">
+          <div class="box_right">
+            <p class="obi">いままでの演奏履歴</p>
+          </div><!-- /box_right -->
+          <div class="cb"></div>
+            <div class="request">
+                	
+                	<div class="quartet"><img src="images/violin/Ｄjyanru01.jpg" />
+                    </div>
+
+              <p class="request_sentence">
+              <b>プロの演奏家をお探しの方はHaus-de-musik Aoyamaへ！</b><br /><br />
+プロの演奏家の出張演奏いたしています。<br />
+                桐朋、芸大卒などの音楽仲間と伺えたら・・・と思っています。<br />
+                演奏に対しては厳しく鍛えられ、きちんとした気持ちで臨むことを学んできた仲間ばかりですので、安心してお任せください。<br />
+                <br />
+                新曲も含め、あらゆるジャンル(クラシック、ポピュラー、日本の曲、アニメソング、童謡、ジャズ、タンゴなど)の曲に、楽譜を入手後、数日で本番に臨めます。<br />
+                <br />
+                演奏時間、構成などはご希望に合わせますのでご相談ください。<br />
+                大切な場の雰囲気にふさわしい演奏となるよう、様々なレパートリーの中から選曲いたします。リクエストがある場合には、どうぞお気軽にお問い合わせください。<br />
+                <br />
+                各種パーティー、宴会、レセプションに多数の出演実績があり、様々な著名アーティストのレコーディング、コンサートのバックミュージシャンとしての参加経験多数。<br />
+                <br />
+                スタジオやコンサートホールでのレコーディング、ライブハウス、レストラン、大使館、迎賓館、公園、ドームなどでの出演多数。</p>
+</div><!-- /sentence01 -->
+            </div><!-- /box02 -->
+        
+      </div><!-- /box_main -->
+    </div><!-- /big_box -->
+    </div><!-- /innner -->
+  <? require_once 'partials/footer.php'; ?> 
+  </div><!-- /contents -->
+</div><!-- /wrapper -->
+</body>
+</html>
