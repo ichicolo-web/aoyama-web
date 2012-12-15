0ecbae4e9510879a837d5490ce8d9a4dbf59713e
ishikawa-h <ishikawa@toshin-k.com>
2012-12-15 22:20:14 +0900

    データベース周り修正

--- admin/violin/delete.php
+++ admin/violin/delete.php
@@ -5,15 +5,15 @@ init();
 
 $confirm_delete = htmlspecialchars($_POST['delete']);
 if (preg_match("/^[0-9]+$/", $confirm_delete)) {
-  $link = mysql_connect('localhost', 'root', 'root');
-  $db_selected = mysql_select_db('update', $link);
+  $link = mysql_connect('sddb0040086768.cgidb', 'sd_dba_ODI4MzQ2', 'XahJtrWz');
+  $db_selected = mysql_select_db('sddb0040086768', $link);
   $sql = "DELETE FROM `posts` WHERE id = $confirm_delete";
   $delete = mysql_query($sql);
   $result_flag = mysql_query($sql);
   $close_flag = mysql_close($link);
-  header("Location: admin.php");
+  header("Location: index.php");
 } else {
   $_SESSION["error_message"] = "削除できませんでした。半角数字を入力してください。";
-  header("Location: admin.php");
+  header("Location: index.php");
 }
 ?>
