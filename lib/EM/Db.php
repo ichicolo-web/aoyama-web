0d41a423732a96f5400c7d4401ae6158999e0400
ishikawa-h <ishikawa@toshin-k.com>
2013-03-20 02:26:59 +0900

    db

--- lib/EM/Db.php
+++ lib/EM/Db.php
@@ -23,6 +23,7 @@ $pass     = 'root';  // パスワード
 try {
   $pdo = new PDO(
       "mysql:host=localhost; dbname=update", "root", "root");
+      //"mysql:host=sddb0040086768.cgidb; dbname=sddb0040086768", "sd_dba_ODI4MzQ2", "XahJtrWz");
 } catch (PDOException $e) {
     var_dump($e->getMessage());
 }
