d00f1ee19224146236028ce1bbb6bd0542828b34
ishikawa-h <ishikawa@toshin-k.com>
2013-03-26 03:09:35 +0900

    update修正

--- lib/EM/Insert.php
+++ lib/EM/Insert.php
@@ -11,13 +11,14 @@ $confirm_title = htmlspecialchars($_POST['check_title']);
 $view_description = htmlspecialchars($_POST['check_description']);
 $confirm_description = nl2br($view_description);
 
-$sql = 'INSERT INTO ' .$db. ' (date,file,writer,title,description) VALUES (' .$confirm_date. ',' .$confirm_filename. ',' .$confirm_writer. ',' .$confirm_title. ',' .$confirm_description. ')';
+$sql = 'INSERT INTO ' .$db. ' (date,file,writer,title,description) VALUES ("' .$confirm_date. '","' .$confirm_filename. '","' .$confirm_writer. '","' .$confirm_title. '","' .$confirm_description. '")';
+echo $sql;
 $result = mysql_query($sql);
 
-  if ($db == 'atelier') {
-    header("Location: /admin/atelier/index.php");
-  } else if ($db == 'posts_piano') {
-    header("Location: /admin/piano/index.php");
-  } else {
-    header("Location: /admin/violin/index.php");
-  }
+if ($db == 'atelier') {
+  header("Location: /admin/atelier/index.php");
+} else if ($db == 'posts_piano') {
+  header("Location: /admin/piano/index.php");
+} else {
+  header("Location: /admin/violin/index.php");
+}
