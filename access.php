c34c7af700bbe0384a046047a195eac9e583ace0
kobitoino <kobito0826@gmail.com>
2013-06-11 00:25:44 +0900

    access修正

--- access.php
+++ access.php
@@ -38,11 +38,13 @@
             <li class="indent">追浜駅より「湘南鷹取循環」または「鷹取一丁目」バスに乗車。 「鷹取中学校前」下車徒歩５分。</li>
             <li class="indent">内川橋ー安浦利用の場合は「追浜一丁目」のバス停下車徒歩７分。</li>
             <li class="indent">※お車の方は駐車場スペースあり。</li>
-            <li class="indent">※詳しい場所は電話でお問い合わせ下さい。</li>
+            <li class="indent">※詳しい住所は電話でお問い合わせ下さい。</li>
           </ul>
         </p>
-        
+        <img src="images/map/takatori_map.jpg">
+        <!--
         <iframe width="948" height="598" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=ja&amp;geocode=&amp;q=35.3054837+139.6180449&amp;aq=&amp;sll=35.305665,139.617965&amp;sspn=0.002469,0.004823&amp;t=m&amp;g=%E6%97%A5%E6%9C%AC%E7%A5%9E%E5%A5%88%E5%B7%9D%E7%9C%8C%E6%A8%AA%E9%A0%88%E8%B3%80%E5%B8%82%E6%B9%98%E5%8D%97%E9%B7%B9%E5%8F%963-5-10&amp;ie=UTF8&amp;ll=35.305484,139.618045&amp;spn=0.009876,0.01929&amp;z=16&amp;iwloc=A&amp;output=embed&amp;iwloc=B"></iframe>
+        -->
         <p>
         <br />
         <span>バイオリン教室(東京教室)</span>
@@ -52,10 +54,13 @@
             <li class="indent">バス系統［荻４０］立教女学院行きバスに乗車。 「久我山」のバス停より徒歩３分。</li>
             <li class="indent">(ＪＲ荻窪、西荻窪駅などからバスが出ています。)</li>
             <li class="indent">※お車の方は駐車場スペースあり。</li>
-            <li class="indent">※詳しい場所は電話でお問い合わせ下さい。</li>
+            <li class="indent">※詳しい住所は電話でお問い合わせ下さい。</li>
           </ul>
         </p>
+        <img src="images/map/kugayama_map.jpg">
+        <!--
        <iframe width="948" height="598" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.co.jp/maps?f=q&amp;source=s_q&amp;hl=ja&amp;geocode=&amp;q=%E6%9D%89%E4%B8%A6%E5%8C%BA%E4%B9%85%E6%88%91%E5%B1%B1%EF%BC%94%E4%B8%81%E7%9B%AE&amp;aq=&amp;sll=35.673343,139.710388&amp;sspn=0.341373,0.559616&amp;brcurrent=3,0x6018ee1e76d820a7:0xc3f631917fdc3b63,1&amp;ie=UTF8&amp;hq=&amp;hnear=%E6%9D%B1%E4%BA%AC%E9%83%BD%E6%9D%89%E4%B8%A6%E5%8C%BA%E4%B9%85%E6%88%91%E5%B1%B1%EF%BC%94%E4%B8%81%E7%9B%AE&amp;t=m&amp;z=16&amp;ll=35.690452,139.597842&amp;output=embed&amp;iwloc=B"></iframe>
+       -->
       </div><!-- /map -->
         <? require_once 'partials/footer.php'; ?> 
     </div><!-- /contents -->
