f0b69fa4312f8eb8b2eb33ce34a8ce346f4462b6
ishikawa <sevens67@i.softbank.jp>
2014-01-25 18:58:44 +0900

    Dbローカル設定

--- lib/EM/Db.php
+++ lib/EM/Db.php
@@ -8,16 +8,14 @@
  ** local
  */
 
-  //$link = mysql_connect('localhost', 'root', 'root');
-  //$db_selected = mysql_select_db('update', $link);
+  $link = mysql_connect('localhost', 'root', 'root');
+  $db_selected = mysql_select_db('update', $link);
 
 /*
  ** production
  */
 
-  //$link = mysql_connect('localhost', 'root', 'root');
-  //$db_selected = mysql_select_db('aoyama_development', $link);
-  $link = mysql_connect('mysql482.db.sakura.ne.jp', 'ichicolo', 'jkh784jkbgdf74dfajk4');
-  $db_selected = mysql_select_db('ichicolo_aoyama', $link);
+  //$link = mysql_connect('mysql482.db.sakura.ne.jp', 'ichicolo', 'jkh784jkbgdf74dfajk4');
+  //$db_selected = mysql_select_db('ichicolo_aoyama', $link);
   
   mysql_set_charset('utf-8');
