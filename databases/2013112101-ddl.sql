51338ceee99d93a3676140d1e5045e38ff1618c5
ishikawa <sevens67@i.softbank.jp>
2014-01-25 18:38:02 +0900

    added databases

---
+++ databases/2013112101-ddl.sql
@@ -0,0 +1,54 @@
+/**
+ * データベーステーブル定義
+ */
+
+DROP TABLE IF EXISTS atelier;
+DROP TABLE IF EXISTS posts_piano;
+DROP TABLE IF EXISTS posts;
+
+-- アトリエ
+
+CREATE TABLE atelier (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT
+  , date TEXT NOT NULL
+  , file TEXT
+  , writer TEXT NOT NULL
+  , title TEXT NOT NULL
+  , description TEXT NOT NULL
+  , created_at TIMESTAMP NOT NULL DEFAULT 0
+  , updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
+) ENGINE = InnoDB
+DEFAULT CHARACTER SET 'utf8';
+ALTER TABLE atelier AUTO_INCREMENT = 1001;
+
+
+-- ピアノ
+
+CREATE TABLE posts_piano (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT
+  , date TEXT NOT NULL
+  , file TEXT
+  , writer TEXT NOT NULL
+  , title TEXT NOT NULL
+  , description TEXT NOT NULL
+  , created_at TIMESTAMP NOT NULL DEFAULT 0
+  , updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
+) ENGINE = InnoDB
+DEFAULT CHARACTER SET 'utf8';
+ALTER TABLE posts_piano AUTO_INCREMENT = 1001;
+
+
+-- バイオリン
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT
+  , date TEXT NOT NULL
+  , file TEXT
+  , writer TEXT NOT NULL
+  , title TEXT NOT NULL
+  , description TEXT NOT NULL
+  , created_at TIMESTAMP NOT NULL DEFAULT 0
+  , updated_at TIMESTAMP NOT NULL DEFAULT NOW() ON UPDATE NOW()
+) ENGINE = InnoDB
+DEFAULT CHARACTER SET 'utf8';
+ALTER TABLE posts_piano AUTO_INCREMENT = 1001;
